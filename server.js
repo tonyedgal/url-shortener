4b165bc1de6da45c532587bda72f025b0de34d2b
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2022-02-10 09:37:57 +0100

    feat: completed all challenges for testing

---
+++ server.js
@@ -0,0 +1,90 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+
+// Basic Configuration
+const port = process.env.PORT || 8000;
+const dbUri = process.env.MONGODB_URI;
+
+app.use(cors());
+
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+// Your first API endpoint
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+let urlSchema = new mongoose.Schema({
+  original: { type: String, required: true },
+  short: Number,
+});
+
+let Url = mongoose.model("Url", urlSchema);
+let urlObject = {};
+
+app.post(
+  "/api/shorturl",
+  bodyParser.urlencoded({ extended: false }),
+  (req, res) => {
+    let inputUrl = req.body["url"];
+    let urlRegex = new RegExp(/^[http://www.]/gi);
+
+    if (!inputUrl.match(urlRegex)) {
+      res.json({ error: "Invaliid URL" });
+      return;
+    }
+
+    urlObject["original_url"] = inputUrl;
+
+    let inputShort = 1;
+
+    Url.findOne({})
+      .sort({ short: "desc" })
+      .exec((error, result) => {
+        if (error) return console.log(error);
+        if (!error && result != undefined) {
+          inputShort = result.short + 1;
+        }
+        if (!error) {
+          Url.findOneAndUpdate(
+            { original: inputUrl },
+            { original: inputUrl, short: inputShort },
+            { new: true, upsert: true },
+            (error, savedUrl) => {
+              if (!error) {
+                urlObject["short_url"] = savedUrl.short;
+                res.json(urlObject);
+              }
+            }
+          );
+        }
+      });
+  }
+);
+
+app.get("/api/shorturl/:input", (req, res) => {
+  let input = req.params.input;
+
+  Url.findOne({ short: input }, (error, result) => {
+    if (!error && result != undefined) {
+      res.redirect(result.original);
+    } else {
+      res.json({ error: "URL not Found" });
+    }
+  });
+});
+
+mongoose
+  .connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(port))
+  .then(() => console.log(`Listening on port ${port}`))
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log(err));
